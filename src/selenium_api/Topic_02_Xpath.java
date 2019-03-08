1b8a097aaa0fd68ab047bc7c8ca4676a796a8359
TUANLQ <tuanlq_tt@runsystem.net>
2019-03-08 18:32:27 +0700

    TOPIC_04 01:59:54

--- src/selenium_api/Topic_02_Xpath.java
+++ src/selenium_api/Topic_02_Xpath.java
@@ -3,6 +3,7 @@ package selenium_api;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -74,23 +75,95 @@ public class Topic_02_Xpath {
 		driver.findElement(By.xpath("//div[@class ='footer']//a[@title='My Account']")).click();
 //		driver.findElement(By.xpath("//div[@class ='footer']//a[contains(text(),'My Account')]")).click();
 //		driver.findElement(By.xpath("//div[@class ='footer']//a[text()='My Account']")).click();
-		
-		//Step 03: Input blank for UserName/Password
+
+		// Step 03: Input blank for UserName/Password
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("");
 		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("");
-		
-		//Step 04: Click button Login //div[@class='buttons-set']//span[contains(text(),'Login')]
+
+		// Step 04: Click button Login
+		// //div[@class='buttons-set']//span[contains(text(),'Login')]
 		driver.findElement(By.xpath("//div[@class='buttons-set']//span[contains(text(),'Login')]")).click();
-		
-		//Step 05: Verify text fill 
-		
+
+		// Step 05: Verify text fill
+
 		String emailErrorMessage = driver.findElement(By.xpath(" //div[@id='advice-required-entry-email']")).getText();
 		String passErrorMessage = driver.findElement(By.xpath("//div[@id='advice-required-entry-pass']")).getText();
-		
-		System.out.println("TWO VERIYFOUND IS:  "+ emailErrorMessage + " " + passErrorMessage);
-		
+
+		System.out.println("TWO VERIYFOUND IS:  " + emailErrorMessage + " " + passErrorMessage);
+
 		Assert.assertEquals(emailErrorMessage, "This is a required field.");
 		Assert.assertEquals(passErrorMessage, "This is a required field.");
+	}
+
+	@Test
+	public void TC_06_RegisterToSystem() {
+		// Step 01: Access URL
+		driver.get("http://live.guru99.com/");
+
+		// Step 02: Click link "My Account"
+		driver.findElement(By.xpath("//div[@class ='footer']//a[@title ='My Account']")).click();
+//		driver.findElement(By.xpath("//div[@class ='footer']//a[text() ='My Account']")).click();
+//		driver.findElement(By.xpath("//div[@class ='footer']//a[contains(text(),'My Account')]")).click();
+
+		// Step 03: Click CREATE AN ACCOUNT button
+		driver.findElement(By.xpath("//span[contains(text(), 'Create an Account')]")).click();
+//		driver.findElement(By.xpath("//span[text() = 'Create an Account']")).click();
+		
+		// Step 04: Fill data register
+		//First Name
+		driver.findElement(By.xpath("//input[@id='firstname']")).sendKeys("Luong");
+		
+		//Last Name
+		driver.findElement(By.xpath("//input[@id = 'lastname']")).sendKeys("Tuan");
+		
+		//RANDOM_EMAIL
+		Random rd = new Random();
+		int number = rd.nextInt(1000);
+		String randomEmail = "TUAN"+ Integer.toString(number) + "@gmail.com";
+		
+		//Email 
+		driver.findElement(By.xpath("//input[@id='email_address']")).sendKeys(randomEmail);
+		
+		System.out.println("EMAIL IS: " + randomEmail );
+		
+		//Password
+		driver.findElement(By.xpath("//input[@id = 'password']")).sendKeys("123456");
+		
+		//Confirm Password
+		driver.findElement(By.xpath("//input[@id ='confirmation']")).sendKeys("123456");
+		
+		
+		//Step 05: click button
+		driver.findElement(By.xpath("//span[contains(text(),'Register')]")).click();
+		
+		//Step 5_01: Verify Message register success: Thank you for registering with Main Website Store
+		
+		String registerSuccessMessage = driver.findElement(By.xpath("//span[contains(text(),'Thank you for registering with Main Website Store.')]")).getText();
+		
+		Assert.assertEquals(registerSuccessMessage,"Thank you for registering with Main Website Store.");
+		
+		System.out.println("--------Register success--------");
+		
+		//Step 06: Logout 
+		
+		driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[text()='Account']")).click();
+		
+		driver.findElement(By.xpath("//a[@title='Log Out']")).click();
+		
+		System.out.println("--------Logout success--------");
+		
+		//Step 06_01: navigate homepage success
+		String homepageSuccess = driver.findElement(By.xpath("//img[@alt= 'Magento Commerce' and @class = 'small']")).getText();
+		
+		Assert.assertEquals(homepageSuccess, "Magento Commerce");
+		
+		System.out.println("HOME PAGE IS: "+ homepageSuccess);
+		
+		
+		
+		
+		
+
 	}
 
 	@AfterClass
