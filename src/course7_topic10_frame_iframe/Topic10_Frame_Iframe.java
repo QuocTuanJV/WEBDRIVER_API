f9d302024a5333e3b602380ef69fcc844b89c07e
TUANLQ <tuanlq_tt@runsystem.net>
2019-06-07 18:14:35 +0700

    00:17:33 Course 7 - Topic 10

---
+++ src/course7_topic10_frame_iframe/Topic10_Frame_Iframe.java
@@ -0,0 +1,34 @@
+package course7_topic10_frame_iframe;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+
+public class Topic10_Frame_Iframe {
+	
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+	}
+
+	@Test
+	public void TC_01_() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
