a96fbe81610b610be33691bc42c92434d2c98815
TUANLQ <tuanlq_tt@runsystem.net>
2019-06-10 18:36:48 +0700

    00:40:34 Topic 10 course 7

--- src/course7_topic10_frame_iframe/Topic10_Frame_Iframe.java
+++ src/course7_topic10_frame_iframe/Topic10_Frame_Iframe.java
@@ -5,7 +5,9 @@ import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
@@ -24,7 +26,24 @@ public class Topic10_Frame_Iframe {
 	}
 
 	@Test
-	public void TC_01_() {
+	public void TC_01_Iframe() {
+		driver.get("https://www.hdfcbank.com/");
+		
+		WebElement notification1 = driver.findElement(By.xpath("//iframe[@id='destination_publishing_iframe_hdfcbank_0']"));
+		if(notification.isDisplayed()) {
+			driver.switchTo().frame(notification1);
+			driver.findElement(By.xpath("//img[@class='popupCloseButton']")).click();
+			System.out.println("Found Car");
+		}
+		System.out.println("Not Found Car");
+		
+		WebElement notification2 = driver.findElement(By.xpath("//iframe[@id='destination_publishing_iframe_hdfcbank_0']"));
+		if(notification.isDisplayed()) {
+			driver.switchTo().frame(notification1);
+			driver.findElement(By.xpath("//img[@class='popupCloseButton']")).click();
+			System.out.println("Found element");
+		}
+		System.out.println("Not Found");
 	}
 
 	@AfterClass
