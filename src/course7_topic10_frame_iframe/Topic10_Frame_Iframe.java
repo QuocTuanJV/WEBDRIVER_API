1ae92e89bf8799fffba7fe4a701e84f292e53c78
TUANLQ <tuanlq_tt@runsystem.net>
2019-06-17 18:22:46 +0700

    TOPIC 11 - 00:20:22

--- src/course7_topic10_frame_iframe/Topic10_Frame_Iframe.java
+++ src/course7_topic10_frame_iframe/Topic10_Frame_Iframe.java
@@ -1,11 +1,16 @@
 package course7_topic10_frame_iframe;
 
 import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
 import org.testng.annotations.BeforeClass;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -27,24 +32,60 @@ public class Topic10_Frame_Iframe {
 
 	@Test
 	public void TC_01_Iframe() {
-		driver.get("https://www.hdfcbank.com/");
-		
-		WebElement notification1 = driver.findElement(By.xpath("//iframe[@id='destination_publishing_iframe_hdfcbank_0']"));
-		if(notification.isDisplayed()) {
-			driver.switchTo().frame(notification1);
-			driver.findElement(By.xpath("//img[@class='popupCloseButton']")).click();
-			System.out.println("Found Car");
-		}
-		System.out.println("Not Found Car");
-		
-		WebElement notification2 = driver.findElement(By.xpath("//iframe[@id='destination_publishing_iframe_hdfcbank_0']"));
-		if(notification.isDisplayed()) {
-			driver.switchTo().frame(notification1);
-			driver.findElement(By.xpath("//img[@class='popupCloseButton']")).click();
-			System.out.println("Found element");
-		}
-		System.out.println("Not Found");
+		 driver.get("https://www.hdfcbank.com");
+		  //get list iframe
+		  List<WebElement> listNotification = driver.findElements(By.xpath("//iframe[@id='notification']"));
+		  
+		  //case 1: have pop up
+		  if(listNotification.size()>0) {
+			  driver.switchTo().frame(listNotification.get(0));// access iframe by index
+			  clickElementByJavascript(driver.findElement(By.cssSelector("#div-close")), driver);// click close pop up by function javacript
+			  System.out.println("Close pop up success");
+			  
+			  driver.switchTo().defaultContent();// Refresh DOM to catch other iframe
+		  }
+		  
+		  
+		  //case 2: Don't have pop up
+		  
+		  System.out.println("Switch to looking for iframe");
+		  WebElement lookingForIframe = driver.findElement(By.xpath("//div[@class='flipBannerWrap']//iframe"));
+		  driver.switchTo().frame(lookingForIframe);
+		  String messageText = driver.findElement(By.xpath("//span[@id='messageText']")).getText();
+		  Assert.assertEquals(messageText, "What are you looking for?");
+		  
+		  
+		  //Refresh DOM
+		  driver.switchTo().defaultContent();
+		  
+		  //Get Image in Iframe
+		  // get in ifame contain image
+		  WebElement slidingIframe = driver.findElement(By.xpath("//div[@class='slidingbanners']//iframe"));
+		  driver.switchTo().frame(slidingIframe);
+		  
+		  //get xpath image in iframe
+		  List<WebElement> bannerImg = driver.findElements(By.xpath("//div[@id='bannercontainer']//img[@class='bannerimage']"));
+		  int bannerImgNumber = bannerImg.size();
+		  
+		  Assert.assertEquals(6, bannerImgNumber);
+		  
+		  //Refresh DOM
+		  driver.switchTo().defaultContent();
+		  
+		  List<WebElement> imgDisplay = driver.findElements(By.xpath("//div[@class='flipBanner']//img[@class='front icon']"));
+		  
+		  for(WebElement img: imgDisplay) {
+		 
+			  Assert.assertTrue(img.isDisplayed());
+			  System.out.println(img.isDisplayed());
+		  }
+		  
 	}
+	
+	public void clickElementByJavascript(WebElement element, WebDriver driver) {
+		  JavascriptExecutor je =(JavascriptExecutor) driver;
+		  je.executeScript("arguments[0].click()", element);
+	  }
 
 	@AfterClass
 	public void afterClass() {
---
+++ src/course7_topic10_frame_iframe/Topic11_Handle_Window_Newtab.java
@@ -0,0 +1,33 @@
+package course7_topic10_frame_iframe;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+import org.testng.annotations.BeforeClass;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+
+
+public class Topic11_Handle_Window_Newtab {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
