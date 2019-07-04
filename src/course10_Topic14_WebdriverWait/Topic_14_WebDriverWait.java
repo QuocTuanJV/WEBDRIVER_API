57bcce6f9ec8202d497c21d802a7b854201582c1
TUANLQ <tuanlq_tt@runsystem.net>
2019-07-04 17:23:45 +0700

    Add them video 14

--- src/course10_Topic14_WebdriverWait/Topic_14_WebDriverWait.java
+++ src/course10_Topic14_WebdriverWait/Topic_14_WebDriverWait.java
@@ -1,10 +1,13 @@
 package course10_Topic14_WebdriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -21,10 +24,85 @@ public class Topic_14_WebDriverWait {
 		driver.manage().window().maximize();
 	}
 
-	@Test
+//	@Test
+	public void TC_01_ImplicitWait() {
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.findElement(By.xpath("//button[text()='Start']")).click();
+		System.out.println(driver.findElement(By.xpath("//div[@id='finish']/h4")).getText());
+
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='finish']/h4")).isDisplayed());
+
+	}
+
+//	@Test
 	public void TC_02_ExplicitWait() {
 		waitExplicit.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("")));
 		waitExplicit.until(ExpectedConditions.presenceOfElementLocated(By.xpath("")));
+
+	}
+
+	@Test
+	public void TC_03_Visible_Presence() {
+
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+		// check visible button click
+		waitExplicit.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//button[text()='Start']")));
+		System.out.println("VISIBLE BUTTON CLICK");
+
+		driver.findElement(By.xpath("//button[text()='Start']")).click();
+
+		// presence
+		waitExplicit.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='loading']")));
+		System.out.println("PRESENCE");
+
+//		// visible
+//		waitExplicit.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@id='loading']")));
+//		System.out.println("VISIBLE");
+//		
+		// check invisible loading or display HelloWorld
+		waitExplicit.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@id='loading']")));
+		System.out.println("INVISIBLE LOADING....");
+		waitExplicit.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//button[text()='Start']")));
+		System.out.println("VISIBLE HELLO WORLD");
+
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='finish']/h4")).isDisplayed());
+
+	}
+	
+	@Test
+	public void TC_04_Visible_Presence() {
+		
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+		
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		
+		// check visible button click
+		waitExplicit.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//button[text()='Start']")));
+		System.out.println("VISIBLE BUTTON CLICK");
+		
+		driver.findElement(By.xpath("//button[text()='Start']")).click();
+		
+		// presence
+		waitExplicit.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='loading']")));
+		System.out.println("PRESENCE");
+		
+//		// visible
+//		waitExplicit.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@id='loading']")));
+//		System.out.println("VISIBLE");
+//		
+		// check invisible loading or display HelloWorld
+		waitExplicit.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@id='loading']")));
+		System.out.println("INVISIBLE LOADING....");
+		waitExplicit.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//button[text()='Start']")));
+		System.out.println("VISIBLE HELLO WORLD");
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='finish']/h4")).isDisplayed());
 		
 	}
 
--- src/selenium_api/Topic_05_Handle_Dropdown_List.java
+++ src/selenium_api/Topic_05_Handle_Dropdown_List.java
@@ -20,16 +20,16 @@ import org.testng.annotations.AfterClass;
 public class Topic_05_Handle_Dropdown_List {
 	WebDriver driver;
 	WebDriverWait waitExplicit;
-	
+
 	JavascriptExecutor javaExecutor;
 
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
 		driver = new ChromeDriver();
-		
+
 		waitExplicit = new WebDriverWait(driver, 60);
-		
+
 		javaExecutor = (JavascriptExecutor) driver;
 
 //		driver = new FirefoxDriver();
@@ -39,133 +39,134 @@ public class Topic_05_Handle_Dropdown_List {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
-	@Test
+//	@Test
 	public void TC_01_Handle_HTML_DropDownlist() {
-	
-		//Go to  Link
-		//Step 01:
+
+		// Go to Link
+		// Step 01:
 		driver.get("https://daominhdam.github.io/basic-form/index.html");
-		
-		//Step 02: Check drop down Job Role 01 not support attribute multi - select
-		
-		Select  select = new Select(driver.findElement(By.xpath("//select[@name ='user_job1']")));
+
+		// Step 02: Check drop down Job Role 01 not support attribute multi - select
+
+		Select select = new Select(driver.findElement(By.xpath("//select[@name ='user_job1']")));
 		Assert.assertFalse(select.isMultiple());
 		System.out.println("DROP DOWN JOB ROLE 01 NOT SUPPORT MULTI - SELECT");
-		
-		//STEP 03: Select value Automation Tester in drop down on method select Visible
+
+		// STEP 03: Select value Automation Tester in drop down on method select Visible
 		select.selectByVisibleText("Automation Tester");
-		
-		//STEP 04: Check successfully select
+
+		// STEP 04: Check successfully select
 		Assert.assertEquals(select.getFirstSelectedOption().getText(), "Automation Tester");
 		System.out.println("SELECT AUTOMATION TESTER SUCCESS");
-		
+
 //		try {
 //			Thread.sleep(3000);
 //		} catch (InterruptedException e) {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-		
-		//STEP 05: SELECT VALUE MANUAL TESTER IN DROP DOWN ON METHOD SELECT VALUE
+
+		// STEP 05: SELECT VALUE MANUAL TESTER IN DROP DOWN ON METHOD SELECT VALUE
 		select.selectByValue("manual");
-		
-		//STEP 06: CHEKC SUCCESSFULLY SELECT
+
+		// STEP 06: CHEKC SUCCESSFULLY SELECT
 		Assert.assertEquals(select.getFirstSelectedOption().getText(), "Manual Tester");
 		System.out.println("SELECT MANUAL TESTER SUCCESS");
-		
+
 //		try {
 //			Thread.sleep(3000);
 //		} catch (InterruptedException e) {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-		
-		//STEP 07: SELECT MOBILE TESTER IN DROP DOWN ON METHOD SELECT INDEX
+
+		// STEP 07: SELECT MOBILE TESTER IN DROP DOWN ON METHOD SELECT INDEX
 		select.selectByIndex(3);
-		
-		//STEP 08: CHECK SUCCESSFULLY SELECT
+
+		// STEP 08: CHECK SUCCESSFULLY SELECT
 		Assert.assertEquals(select.getFirstSelectedOption().getText(), "Mobile Tester");
 		System.out.println("SELECT MOBILE TESTER SUCCESS");
-		
-		//STEP 09: CHECK DROP DOWN HAVE FIVE VALUE
+
+		// STEP 09: CHECK DROP DOWN HAVE FIVE VALUE
 		Assert.assertEquals(select.getOptions().size(), 5);
 		System.out.println("DROP DOWN HAVE FIVE VALUE");
-		
+
 	}
-	
+
 	@Test
 	public void TC_02_Handle_Custom_Dropdown_List() throws InterruptedException {
-		
-		//Jquery
-		//STEP 01: 
+
+		// Jquery
+		// STEP 01:
 		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
-		
-		//STEP 02:
+
+		// STEP 02:
 		selectItemFromCustomDropDownList("//span[@id='number-button']", "//li[@class='ui-menu-item']/div", "19");
-		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()='19']")).isDisplayed());
+		Assert.assertTrue(driver
+				.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()='19']"))
+				.isDisplayed());
 		System.out.println("ITEM: 19 IS SELECTED");
-		
+
 		Thread.sleep(200);
-		
+
 		selectItemFromCustomDropDownList("//span[@id='number-button']", "//li[@class='ui-menu-item']/div", "15");
-		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()='15']")).isDisplayed());
+		Assert.assertTrue(driver
+				.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()='15']"))
+				.isDisplayed());
 		System.out.println("ITEM: 15 IS SELECTED");
-		
+
 		Thread.sleep(200);
-		
+
 		selectItemFromCustomDropDownList("//span[@id='number-button']", "//li[@class='ui-menu-item']/div", "14");
-		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()='14']")).isDisplayed());
+		Assert.assertTrue(driver
+				.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()='14']"))
+				.isDisplayed());
 		System.out.println("ITEM: 14 IS SELECTED");
-		
+
 		Thread.sleep(200);
-		
-		//Kendo UI
-		/* 
-		 DO AFTER
+
+		// Kendo UI
+		/*
+		 * DO AFTER
 		 */
-		
-		//Angular 2
-		
+
+		// Angular 2
+
 		driver.get("https://material.angular.io/components/select/examples");
-		selectItemFromCustomDropDownList("//mat-select[@role ='listbox' and @id='mat-select-5']", "//mat-option/span", "Washington");
-		Assert.assertTrue(driver.findElement(By.xpath("//mat-select[@role ='listbox' and @id='mat-select-5']//span[contains(text(),'Washington')]")).isDisplayed());
-		
-		//Vue JS 2
+		selectItemFromCustomDropDownList("//mat-select[@role ='listbox' and @id='mat-select-5']", "//mat-option/span",
+				"Washington");
+		Assert.assertTrue(driver
+				.findElement(By.xpath(
+						"//mat-select[@role ='listbox' and @id='mat-select-5']//span[contains(text(),'Washington')]"))
+				.isDisplayed());
+
+		// Vue JS 2
 		driver.get("https://mikerodham.github.io/vue-dropdowns/");
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
-	
+
 	public void selectItemFromCustomDropDownList(String parentXpath, String childXpath, String expected) {
 		WebElement element = driver.findElement(By.xpath(parentXpath));
 		javaExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
 		element.click();
-		
+
 		System.out.println("CLICK SUCCESSFULLY!!!");
-		
-		List <WebElement> childList = driver.findElements(By.xpath(childXpath));
+
+		List<WebElement> childList = driver.findElements(By.xpath(childXpath));
 		waitExplicit.until(ExpectedConditions.visibilityOfAllElements(childList));
-		
-		
-		for(WebElement child: childList) {
+
+		for (WebElement child : childList) {
 			String itemSelect = child.getText();
-			
+
 //			System.out.println("ITEM IS: " + itemSelect);
-			if(itemSelect.equals(expected))
-			{
+			if (itemSelect.equals(expected)) {
 				javaExecutor.executeScript("arguments[0].scrollIntoView(true);", child);
 				child.click();
 				break;
 			}
-				
+
 		}
-		
+
 	}
 
 	@AfterClass
